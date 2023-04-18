8456a391f72e0efba3df3dbcbdf0f1dc06221365
Linda_pm <plusmeow218@gmail.com>
2023-04-18 12:16:40 +0800

    專案內容頁完成

--- js/product.js
+++ js/product.js
@@ -25,4 +25,15 @@ $(function(){
     $(this).addClass('active').siblings().removeClass('active')
     $('.content').eq(num).removeClass('hidden').siblings('.content').addClass('hidden')
   })
+
+  $('.amount span').on('click',function(){
+    $(this).removeClass('items').siblings('span').addClass('items')
+    $(this).addClass('border-primary-300 bg-primary-400 text-graay-600 ').siblings('span').removeClass('border-primary-300 bg-primary-400 text-graay-600')
+    if($(this).hasClass('custom')){
+      $('.amount').next('input').removeClass('hidden')
+    }else{
+      $('.amount').next('input').addClass('hidden').val('')
+      // $('.customDonate')
+    }
+  })
 })
